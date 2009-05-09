85a46ff0bd1e2379eff0493ff628cafc1380641b
Martin Kuehl <martin.kuehl@gmail.com>
2009-05-09 18:42:48 +0200

    Implement support for descending into smart folders.
    When pivoting around a saved search, instead of using items from our
    results cache (i.e. other saved searches), we generate a list of results
    from the NSMetadataQuery corresponding to the saved search.

--- SavedSearchesSource.m
+++ SavedSearchesSource.m
@@ -6,20 +6,47 @@
 //
 
 #import <Vermilion/Vermilion.h>
+#import <Vermilion/HGSTokenizer.h>
 #import "HGSDirectoryScannerSearchSource.h"
 
 #pragma mark Static Data
 
+static NSString *const kSavedSearchesResultType
+  = HGS_SUBTYPE(kHGSTypeFileSearch, @"savedSearch");
+
+static NSString *const kSavedSearchesAttributeQueryKey
+  = @"SavedSearchesAttributeQuery";
+
 static NSString *const kSavedSearchesPathComponent = @"Saved Searches";
 static NSString *const kSavedSearchesPathExtension = @"savedSearch";
+
+static NSString *const kSavedSearchesFileQueryKey = @"RawQuery";
+static NSString *const kSavedSearchesFileScopesKey
+  = @"RawQueryDict.SearchScopes";
 
 #pragma mark -
 #pragma mark Helper Functions
 
 static NSString *_SavedSearchesPath(void)
 {
-  return [[NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES)
-           lastObject] stringByAppendingPathComponent:kSavedSearchesPathComponent];
+  NSArray *paths
+    = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory,
+                                          NSUserDomainMask, YES);
+  return [[paths lastObject]
+          stringByAppendingPathComponent:kSavedSearchesPathComponent];
+}
+
+static NSArray *_SavedSearchesQueryScopes(NSDictionary *attrs)
+{
+  return [attrs valueForKeyPath:kSavedSearchesFileScopesKey];
+}
+
+static NSPredicate *_SavedSearchesQueryPredicate(NSDictionary *attrs)
+{
+  NSString *format = [attrs valueForKeyPath:kSavedSearchesFileQueryKey];
+  format = [format stringByReplacingOccurrencesOfString:@" && (true)"
+                                             withString:@""];
+  return [NSPredicate predicateWithFormat:format];
 }
 
 #pragma mark -
@@ -36,6 +63,87 @@ static NSString *_SavedSearchesPath(void)
   NSString *path = _SavedSearchesPath();
   self = [super initWithConfiguration:configuration rootPath:path];
   return self;
+}
+
+#pragma mark NSMetadataQuery
+
+- (void) startQuery:(NSMetadataQuery *)query forPath:(NSString *)path
+{
+  NSDictionary *attrs = [NSDictionary dictionaryWithContentsOfFile:path];
+  if (attrs == nil) {
+    HGSLogDebug(@"%@: Failed to read dictionary from file: %@", self, path);
+  } else {
+    [query setSearchScopes:_SavedSearchesQueryScopes(attrs)];
+    [query setPredicate:_SavedSearchesQueryPredicate(attrs)];
+    [query startQuery];
+  }
+}
+
+#pragma mark NSMetadataItem
+
+- (HGSResult *) resultForMetadataItem:(NSMetadataItem *)mdResult
+                                query:(HGSQuery *)query
+{
+  NSString *path = [mdResult valueForAttribute:(NSString *)kMDItemPath];
+  HGSMutableResult *result = [HGSMutableResult resultWithFilePath:path
+                                                           source:self
+                                                       attributes:nil];
+  NSString *tokenizedName = [HGSTokenizer tokenizeString:[result displayName]];
+  NSString *normalizedQuery = [query normalizedQueryString];
+  [result setRank:HGSScoreForAbbreviation(tokenizedName, normalizedQuery, NULL)];
+  return result;
+}
+
+#pragma mark HGSResult
+
+// Override -indexResult: to add our data to the HGSResult.
+// The added data consists of:
+// - a more specific result type than "file"
+// - an NSMetadataQuery instance executing the saved search
+// Creates a new HGSResult instance and merges it with the given one, because
+// HGSResult's -setValue:forKey: currently contains a failing assertion and
+// cannot be called.
+- (void)indexResult:(HGSResult *)hgsResult
+{
+  NSMetadataQuery *query = [[NSMetadataQuery new] autorelease];
+  NSDictionary *attrs
+    = [NSDictionary dictionaryWithObject:query
+                                  forKey:kSavedSearchesAttributeQueryKey];
+  NSURL *url = [hgsResult url];
+  HGSResult *result = [HGSResult resultWithURL:url
+                                          name:[hgsResult displayName]
+                                          type:kSavedSearchesResultType
+                                        source:self
+                                    attributes:attrs];
+  [super indexResult:[result mergeWith:hgsResult]];
+  [self startQuery:query forPath:[url path]];
+}
+
+#pragma mark HGSSearchSource
+
+- (void) performSearchOperation:(HGSSearchOperation*)operation
+{
+  HGSQuery *query = [operation query];
+  HGSResult *pivot = [query pivotObject];
+  if (pivot) {
+    NSMetadataQuery *mdQuery
+      = [pivot valueForKey:kSavedSearchesAttributeQueryKey];
+    if (mdQuery) {
+      NSMutableArray *results = [NSMutableArray array];
+      [mdQuery disableUpdates];
+      for (NSMetadataItem *mdResult in [mdQuery results]) {
+        HGSResult *result = [self resultForMetadataItem:mdResult query:query];
+        if ([result rank] > 0) {
+          [results addObject:result];
+        }
+      }
+      [mdQuery enableUpdates];
+      [operation setResults:results];
+    }
+  }
+  else {
+    [super performSearchOperation:operation];
+  }
 }
 
 @end
