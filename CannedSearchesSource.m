8006c164abb4bf3cab4d4ab1064d224c7379aeb7
Martin Kuehl <martin.kuehl@gmail.com>
2009-05-09 21:50:02 +0200

    Turn the CannedSearches source into an AbstractSource subclass.

--- CannedSearchesSource.m
+++ CannedSearchesSource.m
@@ -6,36 +6,77 @@
 //
 
 #import <Vermilion/Vermilion.h>
-#import "HGSDirectoryScannerSearchSource.h"
+#import "SavedSearchesAbstractSource.h"
 
-#pragma mark Static Data
+#pragma mark HGSResult Type
+static NSString *const kCannedSearchesResultType
+  = HGS_SUBTYPE(kHGSTypeFileSearch, @"cannedSearch");
 
+#pragma mark Saved Search Path data
 static NSString *const kCannedSearchesBundleIdentifier = @"com.apple.Finder";
 static NSString *const kCannedSearchesPathComponent = @"CannedSearches";
 static NSString *const kCannedSearchesPathExtension = @"cannedSearch";
+static NSString *const kCannedSearchesPathSearchFile = @"search.savedSearch";
+
+#pragma mark Saved Search Content keys
+static NSString *const kCannedSearchesFileQueryKey = @"RawQuery";
+static NSString *const kCannedSearchesFileScopesKey
+  = @"RawQueryDict.SearchScopes";
 
 #pragma mark -
 #pragma mark Helper Functions
 
 static NSString *_CannedSearchesPath(void)
 {
-  return [[[NSBundle bundleWithPath:[[NSWorkspace sharedWorkspace]
-                                     absolutePathForAppBundleWithIdentifier:kCannedSearchesBundleIdentifier]]
-           resourcePath] stringByAppendingPathComponent:kCannedSearchesPathComponent];
+  NSWorkspace *ws = [NSWorkspace sharedWorkspace];
+  NSString *path
+    = [ws absolutePathForAppBundleWithIdentifier:kCannedSearchesBundleIdentifier];
+  path = [[NSBundle bundleWithPath:path] resourcePath];
+  return [path stringByAppendingPathComponent:kCannedSearchesPathComponent];
+}
+
+static NSArray *_CannedSearchesQueryScopes(NSDictionary *attrs)
+{
+  return [attrs valueForKeyPath:kCannedSearchesFileScopesKey];
+}
+
+static NSPredicate *_CannedSearchesQueryPredicate(NSDictionary *attrs)
+{
+  NSString *format = [attrs valueForKeyPath:kCannedSearchesFileQueryKey];
+  format = [format stringByReplacingOccurrencesOfString:@" && (true)"
+                                             withString:@""];
+  return [NSPredicate predicateWithFormat:format];
 }
 
 #pragma mark -
-@interface CannedSearchesSource : HGSDirectoryScannerSearchSource
+@interface CannedSearchesSource : SavedSearchesAbstractSource
 @end
 
 #pragma mark -
 @implementation CannedSearchesSource
 
+#pragma mark Memory Management
 - (id) initWithConfiguration:(NSDictionary *)configuration
 {
-  NSString *path = _CannedSearchesPath();
-  self = [super initWithConfiguration:configuration rootPath:path];
+  self = [super initWithConfiguration:configuration
+                             rootPath:_CannedSearchesPath()
+                           resultType:kCannedSearchesResultType];
   return self;
+}
+
+#pragma mark NSMetadataQuery
+
+- (void) startQuery:(NSMetadataQuery *)query forPath:(NSString *)path
+{
+  path = [path stringByAppendingPathComponent:kCannedSearchesPathSearchFile];
+  NSDictionary *attrs = [NSDictionary dictionaryWithContentsOfFile:path];
+  if (attrs == nil) {
+    HGSLogDebug(@"%@: Failed to read dictionary from file: %@", self, path);
+  } else {
+    [query setSearchScopes:_CannedSearchesQueryScopes(attrs)];
+    [query setPredicate:_CannedSearchesQueryPredicate(attrs)];
+    [query startQuery];
+  }
 }
 
 @end
